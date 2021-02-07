35e4f02faefe21f67033249354d71203c645d36b
elarmandoxd <amd.ilustraciones@gmail.com>
2021-02-06 23:33:23 -0600

    Duck

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 // @ts-nocheck
 const Discord = require('discord.js');
+const express = require('express');
+const app = expres();
 const config = require('./config/config');
 const ytdl = require('ytdl-core');
 const client = new Discord.Client();
 const CronJob = require('cron').CronJob;
 
+const port = 3000;
+
+app.listen(process.env.PORT || port)
+
 client.on('ready', () => {
+  console.log(`
+──────────▄██▄▄
+─▄▄█████▄─██▀
+▀█████████▄██▄
+▒▒▀██████████▀▒ 
+▒▒▒▒▒█▄█▄▄▒▒▒▒▒
+
+█ █▄░█ █▄▀ █▄█   █▀▀ ▄▀█ █▀█ ▀█▀ ▄▀█ █ █▄░█
+█ █░▀█ █░█ ░█░   █▄▄ █▀█ █▀▀ ░█░ █▀█ █ █░▀█
+
+Hora Nacional v 1.0.0
+
+https://www.youtube.com/channel/UCPK3kiwEbpd2m5oGkA2Go8g
+  `)
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 var job = new CronJob('00 00 00 * * 0-6', async function() {
     console.log('Himno Time!');
-    let channel = client.channels.cache.get('720863426930540607');
+    let channel = client.channels.cache.get('807844491977424926');
     let connection = await channel.join();
     var dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=cDAjDDdPqvA&ab_channel=HimnosdeM%C3%A9xico', { filter: 'audioonly' }));
     dispatcher.setVolume(0.5); // half the volume
