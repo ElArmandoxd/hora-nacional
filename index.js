6a12c3fce29ce73c58211c03fc4d27b1c365c9ad
elarmandoxd <amd.ilustraciones@gmail.com>
2021-02-08 11:23:40 -0600

    Himno time

--- index.js
+++ index.js
@@ -11,43 +11,42 @@ const port = 3000;
 
 client.on('ready', () => {
   console.log(`
-──────────▄██▄▄
-─▄▄█████▄─██▀
-▀█████████▄██▄
-▒▒▀██████████▀▒ 
-▒▒▒▒▒█▄█▄▄▒▒▒▒▒
+                                  ▄██▄▄
+                        ▄▄█████▄  ██▀
+                        ▀█████████▄██▄
+                        ▒▒▀██████████▀▒ 
+                        ▒▒▒▒▒█▄█▄▄▒▒▒▒▒
 
-█ █▄░█ █▄▀ █▄█   █▀▀ ▄▀█ █▀█ ▀█▀ ▄▀█ █ █▄░█
-█ █░▀█ █░█ ░█░   █▄▄ █▀█ █▀▀ ░█░ █▀█ █ █░▀█
+          █ █▄░█ █▄▀ █▄█   █▀▀ ▄▀█ █▀█ ▀█▀ ▄▀█ █ █▄░█
+          █ █░▀█ █░█ ░█░   █▄▄ █▀█ █▀▀ ░█░ █▀█ █ █░▀█
 
-Hora Nacional v 1.0.0
+                      Hora Nacional v 1.8.0
 
 https://www.youtube.com/channel/UCPK3kiwEbpd2m5oGkA2Go8g
   `)
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-var job = new CronJob('00 00 00 * * 0-6', async function() {
+const job = new CronJob('00 00 00 * * 0-6', async function() {
     console.log('Himno Time!');
     let channel = client.channels.cache.get('807844491977424926');
     let connection = await channel.join();
-    var dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=cDAjDDdPqvA&ab_channel=HimnosdeM%C3%A9xico', { filter: 'audioonly' }));
+    let dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=cDAjDDdPqvA&ab_channel=HimnosdeM%C3%A9xico', { filter: 'audioonly' }));
     dispatcher.setVolume(0.5); // half the volume
     dispatcher.on('finish', () => {
         console.log('Finished playing!');
-          dispatcher.destroy();
-          console.log("Se acabó el himno");
+        console.log("Se acabó el himno");
     });
   }, async function () {
-    dispatcher.destroy();
     console.log("Se acabó el himno");
+    return;
   },
   true,
 );
 
 client.on('message', msg => {
   if (msg.content === 'culo') {
-    msg.reply('prestas');
+    msg.reply('Prestas');
   }
 });
 
