bd61ed417f7315f702695818bcc052d7cadc0e70
elarmandoxd <amd.ilustraciones@gmail.com>
2021-03-26 08:59:17 -0600

    First Commit

---
+++ comands/twitch.js
@@ -0,0 +1,6 @@
+//@ts-nocheck
+const axios = require('axios');
+
+const twitch = {
+
+}
--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const ytdl = require('ytdl-core');
 const client = new Discord.Client();
 const CronJob = require('cron').CronJob;
 
+let jwordjail = [{name: "not a name", counter: 0}];
+
 const port = 3000;
 
 client.on('ready', () => {
@@ -29,19 +31,17 @@ client.on('ready', () => {
   channel.leave();
 });
 
-const job = new CronJob('00 00 06 * * 0-6', async function() {
+const job = new CronJob('00 00 04 * * 0-6', async () => {
     console.log('Himno Time!');
     let channel = client.channels.cache.get('807844491977424926');
     let connection = await channel.join();
-    let dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=_yNRCjMpTNg', { filter: 'audioonly' }));
+    let dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=cDAjDDdPqvA', { filter: 'audioonly' }));
     dispatcher.setVolume(0.5); 
     dispatcher.on('finish', () => {
         dispatcher.destroy();
-        console.log("Se acabó el himno");
         channel.leave();
     });
-  }, async function () {
-    console.log("Se acabó el himno");
+  }, async () => {
     dispatcher.destroy();
     channel.leave();
     return;
@@ -49,12 +49,59 @@ const job = new CronJob('00 00 06 * * 0-6', async function() {
   true,
 );
 
+const jailJob = new CronJob('00 00 04 * * Monday', async () =>{
+  
+  }, async()=>{
+
+  },
+  true,
+);
+
+//Culo-prestas function
 client.on('message', msg => {
-  if (msg.content === 'culo') {
-    console.log("Somebody said a bad word!");
-    msg.reply('Prestas');
+  let splited = msg.content.split(' ');
+  for(let i = 0; i < splited.length; i++){
+    let s = splited[i];
+    if(s.toUpperCase() === 'CULO' ) {
+      console.log("Somebody said a bad word!");
+      msg.reply('Prestas');
+      return;
+    }
   }
 });
+
+client.on('message', msg =>{
+  let splited = msg.content.split(' ');
+  for(let i = 0; i < splited.length; i++){
+    let s = splited[i];
+    if(s.toUpperCase() === 'JOTO' ) {
+      homofoboJail(msg);
+      return;
+    }
+  }
+});
+
+function homofoboJail(message){
+  let us = null;
+  for(let i = 0; i < jwordjail.length; i++){
+    if(jwordjail[i].name == message.author.username){
+      jwordjail[i].counter ++;
+      us = jwordjail[i];
+      message.reply('<:PeepoCop:728085789321330708> ¡ALTO AHI '+us.name+ ' HOMOFOBO QLIAO! <:PeepoCop:728085789321330708> Haz dicho la J word '+us.counter + ' veces. Te estaré vigilando.');
+      return;
+    }
+    else if(i == (jwordjail.length-1)){
+      let user = {
+        name : message.author.username,
+        counter: 1
+      }
+      jwordjail.push(user);
+      us = user;
+      message.reply('<:PeepoCop:728085789321330708> ¡ALTO AHI '+us.name+ ' HOMOFOBO QLIAO! <:PeepoCop:728085789321330708> Haz dicho la J word '+us.counter +' vez. Te estaré vigilando.');
+      return;
+    }
+  }
+}
 
 //Landing page
 app.get('/',(req,res)=>{
